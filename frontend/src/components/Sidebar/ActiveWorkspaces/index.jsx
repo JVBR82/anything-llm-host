9004daf084e3b5b80904afeaf60b06a445e5c698
Jorge Vicente Bermúdez Rico <jordi@MacBook-Pro-de-Jorge.local>
2026-04-18 19:44:20 +0200

    Revert "host cannot edit settings for workspaces"
    This reverts commit 84130374fc5f502850b33573f623f76c19cf18c2.

--- frontend/src/components/Sidebar/ActiveWorkspaces/index.jsx
+++ frontend/src/components/Sidebar/ActiveWorkspaces/index.jsx
@@ -142,7 +142,7 @@ export default function ActiveWorkspaces() {
                                 </p>
                               </div>
                             </div>
-                            {user?.role !== "default" && user?.role !== "host" (
+                            {user?.role !== "default" && (
                               <div
                                 className={`flex items-center gap-x-[2px] transition-opacity duration-200 ${isActive ? "opacity-100" : "opacity-0 group-hover:opacity-100"}`}
                               >
